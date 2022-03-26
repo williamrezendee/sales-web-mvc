5646e9c76f22221b50378e387ac307d640ed16d7
William Rezende <williamrezendee@gmail.com>
2022-03-25 21:32:49 -0300

    Criando todas as entidades do projeto e o serviço que popula o db com dados

--- SalesWebMvc/Startup.cs
+++ SalesWebMvc/Startup.cs
@@ -39,14 +39,16 @@ namespace SalesWebMvc
             services.AddDbContext<SalesWebMvcContext>(options =>
                     options.UseMySql(Configuration.GetConnectionString("SalesWebMvcContext"),
                                      builder => builder.MigrationsAssembly("SalesWebMvc")));
+            services.AddScoped<SeedingService>();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env, SeedingService seeding)
         {
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
+                seeding.Seed();
             }
             else
             {
